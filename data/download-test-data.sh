6c43fb6cfc4acb728ce6378c2f2fe1701cb8fe8c
Hernán Ordiales <hordiales@pjn.gov.ar>
2016-07-01 17:30:22 -0300

    * Script to download test data

---
+++ data/download-test-data.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# guitarra
+wget http://redpanal.org/media/uploads/audios/2014_05/126.mp3
+wget http://redpanal.org/media/uploads/audios/2014_05/795.ogg
+wget http://redpanal.org/media/uploads/audios/2014_05/984.ogg
+
+# bajo
+wget http://redpanal.org/media/uploads/audios/2014_05/1194.mp3
+wget http://redpanal.org/media/uploads/audios/2014_05/983.ogg
+wget http://redpanal.org/media/uploads/audios/2014_05/1288.ogg
+wget http://redpanal.org/media/uploads/audios/2014_05/982.ogg
