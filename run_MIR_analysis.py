10699e95b4b8159c9e172451833d4e8fc04bd276
anitalmada <anitalmada@Thelma.Thelma>
2016-08-10 19:07:13 -0300

    Corrige algunos errores y muestra algunos resultados para desarrollar el algoritmo

--- run_MIR_analysis.py
+++ run_MIR_analysis.py
@@ -8,7 +8,7 @@ import json
 from essentia import *
 from essentia.standard import *
 
-files_dir = "guitarra/"
+files_dir = "bajo/"
 ext_filter = ['.mp3','.ogg','.ogg','.wav'] #archivos de sonido válidos
 
 # descriptores de interés
--- similaridades_sonoras.py
+++ similaridades_sonoras.py
@@ -1,18 +1,23 @@
 import numpy as np                                                      
 import matplotlib.pyplot as plt                                   
 import os, sys                                                          
-import json                                                             
+import json    
+import re                                                         
 from sklearn import preprocessing                                       
-from sklearn.cluster import AffinityPropagation()                                     
+from sklearn.cluster import AffinityPropagation                                  
                                                                         
-files_dir = 'descriptores/bajo/'                                            
+files_dir = 'descriptores/guitarra'                                            
                                              
 for subdir, dirs, files in os.walk(files_dir):                          
-    dic = [json.load(open(subdir+f)) for f in files]
+    dic = [json.load(open(subdir+'/'+f)) for f in files]
 
 features = re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", str(dic))     
 files_features = np.array_split(features, len(files))
 min_to_max = preprocessing.MinMaxScaler(feature_range=(-1, 1))          
 lowest_to_highest = min_to_max.fit_transform(files_features)  
 euclidean = AffinityPropagation()                           
-euclidean_labels = model.fit_predict(lowest_to_highest)  
+euclidean_labels = euclidean.fit_predict(lowest_to_highest)  
+
+print files
+print files_features
+print euclidean_labels
