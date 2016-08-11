1a7a0abc012fbbbfe21dce56cffbbe3716497105
anitalmada <anitalmada@Thelma.Thelma>
2016-08-11 00:22:28 -0300

    Plot de similaridades sonoras por propagación de afinidad, muestra los sonidos más ejemplares por agrupamiento

--- similaridades_sonoras.py
+++ similaridades_sonoras.py
@@ -9,15 +9,28 @@ from sklearn.cluster import AffinityPropagation
 files_dir = 'descriptores/guitarra'                                            
                                              
 for subdir, dirs, files in os.walk(files_dir):                          
-    dic = [json.load(open(subdir+'/'+f)) for f in files]
+    details = {}               
+    dics = [json.load(open(subdir+'/'+f)) for f in files]
+    details =  {'file': files, 'features': dics}
 
-features = re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", str(dic))     
-files_features = np.array_split(features, len(files))
+features = re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", str(details['features']))     
+files_features = np.array_split(features, len(files))  
+
+first_descriptor_values = (zip(*files_features)[0])
+second_descriptor_values = (zip(*files_features)[1])
+
 min_to_max = preprocessing.MinMaxScaler(feature_range=(-1, 1))          
-lowest_to_highest = min_to_max.fit_transform(files_features)  
-euclidean = AffinityPropagation()                           
-euclidean_labels = euclidean.fit_predict(lowest_to_highest)  
+lowest_to_highest = min_to_max.fit_transform(np.vstack((first_descriptor_values,second_descriptor_values)).T)   
+euclidean = AffinityPropagation(convergence_iter=10, affinity='euclidean')                           
+euclidean_labels= euclidean.fit_predict(lowest_to_highest)  
 
-print files
-print files_features
-print euclidean_labels
+print "Cada número representa grupos de sonidos ejemplares, observa el ploteo para ver qué sonidos podrían ser ejemplares de otros"
+print np.vstack((euclidean_labels,files)).T
+print lowest_to_highest
+
+plt.scatter(lowest_to_highest[euclidean_labels==0,0], lowest_to_highest[euclidean_labels==0,1], c='b')
+plt.scatter(lowest_to_highest[euclidean_labels==1,0], lowest_to_highest[euclidean_labels==1,1], c='r')
+plt.scatter(lowest_to_highest[euclidean_labels==2,0], lowest_to_highest[euclidean_labels==2,1], c='y')
+plt.xlabel('Loudness Level (scaled)')
+plt.ylabel('Disonancia(scaled)')
+plt.show()
