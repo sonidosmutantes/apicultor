6017a8b3b1deefaa4cca159b792aaca5fc1df2ed
Hernán Ordiales <hordiales@gmail.com>
2016-09-06 12:19:02 -0300

    Run MIR analysis exceptions, error count. Only process sound files (filtering)

---
+++ tests/Test_All.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+from Test_API import *
+from Test_Similarity import *
+
+
+if __name__ == '__main__':
+   #unittest.main()
+    unittest.main(verbosity=2)
--- tests/Test_Similarity.py
+++ tests/Test_Similarity.py
@@ -6,15 +6,13 @@ import sys
 import unittest
 import numpy as np
 
-# sys.path.append(os.path.dirname(os.path.abspath(''))) use this if you've compiled apicultor
-
 # from apicultor.SoundSimilarity import * # import from compiled apicultor 
-
-#sys.path.append(os.path.dirname(os.path.abspath('..'))) #use this if you've compiled apicultor
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath('__file__')), '..'))
 from SoundSimilarity import * # first copy SoundSimilarity to tests directory
 
-files_dir = '/full/path/to/apicultor/apicultor/data/bajo' # full path of sounds tag
+
+#Example
+files_dir = '../data/bajo' # full path of sounds tag
 files = get_files(files_dir)
 dics = get_dics(files_dir)
 
@@ -22,7 +20,7 @@ class Testfiles(unittest.TestCase):
 
     def test_get_files(self):
 	#TODO: assert expected results
-	print("TODO: compare with expected results (add assertions)")
+	    print("TODO: compare with expected results (add assertions)")
         self.assertNotEqual(plot_similarity_clusters(files,dics), -1) # make sure clustering doesn't fail
  
 if __name__ == '__main__':
