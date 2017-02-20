f0529333a91740dd4ec321f892866a444ee9765b
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@yahoo.com>
2017-02-20 02:14:30 -0300

    Update setup.py
    And contact info

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ from distutils.core import setup
 setup(name='apicultor',
       description='Extrae miel de RedPanal',
       author='Hernán Hordiales, Marcelo Tuller',
-      author_email='hordiales@gmail.com, marscrophimself@yahoo.com',
-      packages=['apicultor', 'apicultor.data', 'apicultor.tests'],
+      author_email='hordiales@gmail.com, marscrophimself@protonmail.com',
+      packages=['apicultor', 'apicultor.data', 'apicultor.tests', 'apicultor.utils', 'apicultor.constraints', 'apicultor.gradients'],
      )
