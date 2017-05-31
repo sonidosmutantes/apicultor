70d14d96e10237e905890e3d67a72ba9bcd44e45
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@yahoo.com>
2017-05-31 18:24:29 -0300

    Update time_freq.py

--- constraints/time_freq.py
+++ constraints/time_freq.py
@@ -1,7 +1,7 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from __future__ import division
+
 import numpy as np
 from librosa.core import stft
 from sklearn.utils.extmath import safe_sparse_dot as ssd
@@ -38,7 +38,7 @@ def find_sparse_source_points(real, imag):
     dif = cos_dif(angle)
     X = angle.copy()
     is_sparse = np.abs(tf_plane) > dif                                                 
-    for i in xrange(X.shape[0]):
+    for i in range(X.shape[0]):
         if np.any(is_sparse[i]) == True:        
             X[i][is_sparse[i] == True] = X[i][is_sparse[i] == True]
         else:
@@ -48,7 +48,7 @@ def find_sparse_source_points(real, imag):
 
 def cosine_distance(X):
     cos_dist = []
-    for i in xrange(len(X)):
+    for i in range(len(X)):
         cos_dist.append(1 - np.abs(tf_map(X.T[i- 1], X.T[i])))
     cos_dist = np.array(cos_dist)
     cos_dist[np.isnan(cos_dist)] = 0
@@ -59,10 +59,9 @@ def find_number_of_sources(cosine_distance):
     ap = AP(affinity = 'precomputed').fit(cos_dist)
     counter = Counter(ap.labels_).most_common()
     source = 0
-    for i in xrange(len(counter)):
+    for i in range(len(counter)):
         if counter[i][1] == counter[0][1]:     
             source += 1
     return source
-
 
 
