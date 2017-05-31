205c71477a6bdf77529ce5ba484365ddbe1d457b
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@yahoo.com>
2017-05-31 13:44:04 -0300

    Update Test_MIR_Analysis.py

--- apicultor/tests/Test_MIR_Analysis.py
+++ apicultor/tests/Test_MIR_Analysis.py
@@ -8,14 +8,14 @@ class TestMIRAnalysis(unittest.TestCase):
     def test_error_count_in_data_dir(self):
         directory = "data"
         try:
-            output = subprocess.check_output( "cd .. && ./run_MIR_analysis.py %s"%directory, shell=True, stderr=subprocess.STDOUT)
+            output = subprocess.check_output( "cd .. && ./run_mir_analysis.py %s"%directory, shell=True, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             self.fail("Found errors processing 'data' directory. Error count: %i"%e.returncode)
             
     def test_error_count_in_samples_dir(self):
         directory = "samples"
         try:
-            output = subprocess.check_output( "cd .. && ./run_MIR_analysis.py %s"%directory, shell=True, stderr=subprocess.STDOUT)
+            output = subprocess.check_output( "cd .. && ./run_mir_analysis.py %s"%directory, shell=True, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             self.fail("Found errors processing 'samples' directory. Error count: %i"%e.returncode)
 
