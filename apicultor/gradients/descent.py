8c3a2cfa6d42e5f02b2f63ceb12002cf0e640b4c
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@yahoo.com>
2017-05-31 13:26:33 -0300

    Update descent.py

--- apicultor/gradients/descent.py
+++ apicultor/gradients/descent.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-from __future__ import division
 import numpy as np
 from sklearn.utils.extmath import safe_sparse_dot as ssd
 
 #taken from Pegasos algorithm by avaitla
 def SGD(a, lab, Q, reg_param):
     iterations = 1
-    for i in xrange(7):
-        for tau in xrange(len(a)):
+    for i in range(7):
+        for tau in range(len(a)):
             wx = ssd(a, Q[tau,:], dense_output = True)
             a = a * (1-1/iterations)
             if(lab[tau]*wx < 1):
