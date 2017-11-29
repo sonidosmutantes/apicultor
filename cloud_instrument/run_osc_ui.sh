7eb69ec3c8973c8ce2b304870cb23ef100faa4c7
Hernán Ordiales <hordiales@gmail.com>
2017-11-29 00:39:30 -0300

    ui update

--- cloud_instrument/run_osc_ui.sh
+++ cloud_instrument/run_osc_ui.sh
@@ -3,7 +3,7 @@ UI=apicultor # cloud instrument
 #UI=nube_sinte_complex
 # with -n -> no external UI (browser only)
 #open-stage-control -n -l ui/$UI.js -s 127.0.0.1:5555 127.0.0.1:9001
-open-stage-control -l ui/$UI.js -s 127.0.0.1:9001 -p 7000
+open-stage-control -l ui/$UI.json -s 127.0.0.1:9001 -p 7000
 
 #$ open-stage-control -s 127.0.0.1:5555 127.0.0.1:6666 -p 7777
 #
