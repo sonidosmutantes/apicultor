6183d0a624ed37b4902c8b0599f18a3fc64cb73a
Hernan Ordiales <hordiales@gmail.com>
2018-05-23 01:51:31 +0000

    raspicultor config

--- cloud_instrument/clean_disk.sh
+++ cloud_instrument/clean_disk.sh
@@ -1,11 +1,13 @@
 cd ~/dev/apicultor/cloud_instrument
 
 #STARTUP_FILES="FILE1.wav FILE2.wav"
-STARTUP_FILES="Ride_01-21.wav Basspad.wav"
+STARTUP_FILES="Ride_01-21.wav Basspad.wav instrucciones.wav"
 
 mkdir startupfiles
 mv $STARTUP_FILES startupfiles/
 echo $STARTUP_FILES
+# TODO: uppercase and lowercase
+#rm *.wav*
 rm *.wav* *.aif* *.ogg *.mp3 *.mp4 *.flac
 mv startupfiles/* .
 
