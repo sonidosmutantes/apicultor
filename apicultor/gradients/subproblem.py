9a7a7e0f0bd32e0f6cd70fd60d88a51f4e70b6cc
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@protonmail.com>
2019-03-07 15:12:59 -0300

    Update subproblem.py

--- apicultor/gradients/subproblem.py
+++ apicultor/gradients/subproblem.py
@@ -4,6 +4,7 @@
 from __future__ import division
 import numpy as np
 
+s = lambda smax, b, B: 1/smax+(smax-(1/smax))*((b-1)/(B-1))
 sigmoid = lambda x: 1 / (1 + np.e ** (-1 * x))
 g = lambda lab, a0, Q: 1 - lab * np.sum(a0 * Q)                 
 Q_a = lambda a, Q: np.sum(a) - (np.sum(np.sum(a * Q, axis = 0), axis = 0)/2)
