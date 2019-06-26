52ebec9148b2476d6794b07b23b37100a256c5f9
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@protonmail.com>
2019-06-25 21:20:14 -0300

    Update subproblem.py

--- apicultor/gradients/subproblem.py
+++ apicultor/gradients/subproblem.py
@@ -8,6 +8,6 @@ sigmoid = lambda x: 1 / (1 + np.e ** (-1 * x))
 g = lambda lab, a0, Q: 1 - lab * np.sum(a0 * Q)                 
 Q_a = lambda a, Q: np.sum(a) - (np.sum(np.sum(a * Q, axis = 0), axis = 0)/2)
 def attention(Q, K, V):
-    num = np.array(Q.dot(K.T))
+    num = np.array(Q @ K.T)
     denum = np.sqrt(K.shape[0])
     return sigmoid(num / denum) * V
