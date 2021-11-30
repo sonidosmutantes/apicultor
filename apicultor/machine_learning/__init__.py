5e56045aefc714e82dc15adfa498ab508e10a34c
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@protonmail.com>
2021-11-30 16:58:52 -0300

    Update __init__.py

--- apicultor/machine_learning/__init__.py
+++ apicultor/machine_learning/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['SoundSimilarity', 'quality', 'cross_validation', 'cache']
+__all__ = ['SoundSimilarity', 'quality', 'cross_validation', 'cache', 'dependency', 'explain', 'metrics', 'visuals']
