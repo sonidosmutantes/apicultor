20e4d87b39b2687984cf0e9b558785b03aff86ef
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@protonmail.com>
2021-12-11 13:41:22 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup, Extension
 from setuptools import find_packages
 
-with open('README_es.md', encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 examples = Extension('examples',
@@ -13,25 +13,35 @@ examples = Extension('examples',
 supercollider = Extension('apicultor.supercollider',
                     sources = ['live_coding.sc', 'setup_performance.sc'])
 
-setup(name='apicultor',
-      version='0.1.0',
+setup(name='apicultor-dev',
+      version='2.0.1',
       url='https://www.github.com/sonidosmutantes/apicultor',
-      description='Extrae miel de RedPanal',
+      description='BigData system of sound effects, remixes and sound collections',
       long_description=long_description,
-      author='Hernán Hordiales, Marcelo Tuller',
+      long_description_content_type='text/markdown',
+      author='Hernán Ordiales, Marcelo Tuller',
       author_email='hordiales@gmail.com, marscrophimself@protonmail.com',
+      download_url='https://github.com/sonidosmutantes/apicultor/archive/refs/heads/dev.zip',
+      keywords = ['deeplearning', 'ml', 'mlops', 'svm', 'music', 'dsp', 'sound', 'audio'],      
       packages=find_packages(),
       license='GPLv3',
     classifiers=[
-        'Development Status :: 4 - Beta',
-
-        'Intended Audience :: everyone who likes music!',
-        'Topic :: AI :: Music Information Retrieval :: Collaborative Composing and Remixing',
-
-        'Programming Language :: Python :: 2.7, 3.5',
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'Programming Language :: Python :: 3.9',
     ],
     entry_points={'console_scripts': [
-        'rpdl = apicultor.helper.WebScrapingDownload:main', 'rpdla = apicultor.helper.ArchiveWebScrapingDownload:main', 'miranalysis = apicultor.run_mir_analysis:main', 'musicemotionmachine = apicultor.emotion.MusicEmotionMachine:main', 'sonify = apicultor.sonification.Sonification:main', 'qualify = apicultor.machine_learning.quality:main', 'soundsimilarity = apicultor.machine_learning.SoundSimilarity:main', 'mockrpapi = apicultor.MockRedPanalAPI_service:main', 'audio2ogg = apicultor.helper.convert_to_ogg:main', 'smcomposition = apicultor.state_machine.SMComposition:main'
+        'rpdl = apicultor.helper.WebScrapingDownload:main',
+        'rpdla = apicultor.helper.ArchiveWebScrapingDownload:main',
+        'miranalysis = apicultor.run_mir_analysis:main',
+        'musicemotionmachine = apicultor.emotion.MusicEmotionMachine:main',
+        'sonify = apicultor.sonification.Sonification:main',
+        'qualify = apicultor.machine_learning.quality:main',
+        'soundsimilarity = apicultor.machine_learning.SoundSimilarity:main',
+        'mockrpapi = apicultor.MockRedPanalAPI_service:main',
+        'audio2ogg = apicultor.helper.convert_to_ogg:main',
+        'smcomposition = apicultor.state_machine.SMComposition:main'
     ]},
-    install_requires=['numpy', 'wget', 'colorama', 'pysoundfile', 'librosa', 'scipy', 'matplotlib', 'scikit-learn', 'bs4', 'pandas']
+    install_requires=['numpy', 'numba', 'smst', 'wget', 'colorama', 'transitions','pysoundfile', 'librosa', 'scipy', 'matplotlib', 'scikit-learn', 'bs4', 'pandas']
      )
