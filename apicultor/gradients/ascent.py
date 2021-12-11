969a496de918522aa92ec3f962951c10c2bb60fc
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@protonmail.com>
2021-12-11 14:48:36 -0300

    Add files via upload

--- apicultor/gradients/ascent.py
+++ apicultor/gradients/ascent.py
@@ -1,22 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-from .subproblem import sigmoid, attention
+from .subproblem import s, sigmoid, attention
 import numpy as np
 
-#taken from Pegasos algorithm by avaitla
-def SGD(a, lab, Q, lr):
-    for i in range(20):
-        iterations = 1
-        for tau in range(len(a)):
-            if a[tau] > 0:
-                wx = a @ Q[:,tau]
-                a[tau] *= (1 - 1/iterations)
-                if(lab[tau]*wx < 1):
-                    a[tau] += lab[tau]/(lr * iterations)
-                iterations += 1
-    return a
-
-def attention_sgd(x,y,a=None):
+def attention_sga(x,y,a=None):
     if a == None:
         a = np.zeros(x.shape[1])
     else:
@@ -29,5 +14,5 @@ def attention_sgd(x,y,a=None):
             s_max = 1.5
             s1 = s(1.5,tau,x.shape[1])    
             ql =(s_max * np.abs(np.cosh(s1 * et)+1)) / (s1*np.abs(np.cosh(s1 * et)+1))
-            a[tau] -= ql
+            a[tau] += ql
     return a
