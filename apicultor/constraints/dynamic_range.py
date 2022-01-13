e1b5ad5e9b28a93e260335302ab0d3b6567945d3
Marcelo Tuller (a.k.a. Mars Crop) <marscrophimself@protonmail.com>
2022-01-13 18:52:20 -0300

    Add constraints for noise gate

--- apicultor/constraints/dynamic_range.py
+++ apicultor/constraints/dynamic_range.py
@@ -3,6 +3,15 @@
 
 def dyn_constraint_satis(audio, variables, gain):
     assert ((type(variables) is list) and len(variables) == 2)
-    audio[variables[0] < variables[1]] = gain  #noise gating: anything below a threshold is silenced     
+    #attenuate
+    audio[variables[0] < variables[1]] = gain      
     audio[variables[0] > variables[1]] = 1
+    return audio
+
+def gate_constraint_satis(audio, variables, gain):
+    assert ((type(variables) is list) and len(variables) == 2)
+    #close unfeasible
+    audio[variables[0] < variables[1]] = 0 
+    #keep feasible open
+    audio[variables[0] > variables[1]] = 1   
     return audio
